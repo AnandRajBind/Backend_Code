9453470c37dc1e3ca55482c9ca31fdd4afd618d5
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-06-10 11:59:43 +0530

    10/06/2025
    basic ExpresJs middleware, routes  and request response

---
+++ Express/script.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+// request and response are the two main objects in express.js.
+// request is used to get the data from the client and response is used to send the data to the client. 
+
+// middlware  is a element between server accept the request and before it send the resonse to the routes/client.
+
+app.use(function (req, res, next) {
+    console.log('Middleware function executed');
+    //   res.send('Middleware function executed'); 
+    // Send a response to the client
+     // You can perform any operations here, like logging, authentication, etc.
+    next(); // Call the next middleware or route handler
+});
+app.use(function (req, res, next) {
+    console.log('Middleware function executed second time ');
+    next(); // Call the next middleware or route handler
+});
+
+// creating Routes  
+app.get('/', (req, res) => {
+    res.send('Home Page - Welcome to Express!');
+});
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
+app.get('/', (req, res) => {
+    res.send('About Page');
+});
+app.listen(8000);
+
---
+++ server/script.js
@@ -0,0 +1,7 @@
+const http=require('http');
+
+const server=http.createServer((req,res)=>{
+    res.end("hello kallu");
+})
+
+server.listen(8000);
