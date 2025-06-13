e60dd140caa9348b7b4df1cd480eb32d52ffb25b
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-06-13 08:20:29 +0530

    13/06/2025
    Add features like Click on read more button then open the new ejs file and show the all details of file.

--- Notes project/index.js
+++ Notes project/index.js
@@ -17,20 +17,29 @@ app.get('/', (req, res) => {
     fs.readdir(`./files`, (err, files) => {
         res.render("index.ejs", { files: files });// reddir ka jab callback function call ho tab jaker response render hoga.yaha per render karte time ham views page per kuch bhi send ker sakta hu object ke roop me {files: files}. first files is denoted to kis name se ham files ko send ker rahe hai view page per and second files is the variable which we are sending to the view page. first files is key and second files is value.
     });
-    });
-// here first we read the files from the files folder and then we render the index.ejs file and pass the files to it.
+});
+app.get('/file/:filename', (req, res) => {
+    fs.readFile(`./files/${req.params.filename}`, "utf-8", function (err, fileData) {
+        // console.log(fileData);
+        // filename and filedata name se data jayega show name ke ejs file me.iske bad waha se data ko render karega frontend per.
+        res.render('show',{filename:req.params.filename, fileData:fileData});
+    })
+}
+)
+
+
 // flow. read data from frontend using writefile()->send files to backend using ejs->render the index.ejs file and pass the files to it->display the files on the frontend using ejs.  
-     app.post('/create', (req, res) => {
+app.post('/create', (req, res) => {
 
-        fs.writeFile(`./files/${req.body.title.split('').join('')}.txt`, req.body.details, function (err) {
-    //    console.log(req.body);
-       res.redirect('/');// redirect to the home page after creating a file.
-        })
-    });
+    fs.writeFile(`./files/${req.body.title.split('').join('')}.txt`, req.body.details, function (err) {
+        //    console.log(req.body);
+        res.redirect('/');// redirect to the home page after creating a file.
+    })
+});
 
-    app.listen(3000);
+app.listen(3000);
 
-    
+
 
 
 
