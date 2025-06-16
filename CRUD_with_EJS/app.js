b01e342bfb9f2331e79fe44c9245f127395b47ff
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-06-16 08:53:44 +0530

    16/06/2025
    CRUD operations with EJS and Server Side Rendering Completed.

--- CRUD_with_EJS/app.js
+++ CRUD_with_EJS/app.js
@@ -31,9 +31,23 @@ const users= await userModel.findByIdAndDelete({_id: req.params.id});
     res.redirect("/read"); // Pass the users to the EJS template
 })
 
+
+app.get('/edit/:userid', async (req, res) => {
+ const user=await userModel.findOne({_id: req.params.userid})
+
+res.render("edit",{user}); // Pass the user to the EJS template})
+})
+
+
+app.post('/update/:userid', async (req, res)=>{
+  let {email, name, image}  =req.body; // Destructure the request body to get the user details
+let user= await userModel.findOneAndUpdate({_id: req.params.userid}, {image, name, email}, {new: true});
+    res.redirect('/read'); // Redirect to the read page after updating
+
+})
+
 app.post('/create', async (req, res) => {
- 
-    let{name,email,image}=req.body; // 
+let{name,email,image}=req.body; // 
 const createdUser=await userModel.create({
 name,
 email,
