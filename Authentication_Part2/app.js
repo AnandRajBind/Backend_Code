d095d22a37074cca9a039519c64a87a5595bad38
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-06-17 22:17:29 +0530

    17/06/2025
    Login and Logout with Authentication

--- Authentication_Part2/app.js
+++ Authentication_Part2/app.js
@@ -13,14 +13,15 @@ login-> token-> decrypt -> email
 
 const express = require('express')
 const app = express();
-const cookieParser=require('cookie-parser')
-const path= require('path');
-
-const userModel= require('./models/user.js');
+const cookieParser = require('cookie-parser')
+const path = require('path');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+const userModel = require('./models/user.js');
 
 // form data parsing
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 // cookie parsing
 app.use(cookieParser());
 // static files
@@ -29,16 +30,53 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.render('index.ejs');
-})
-app.post('/create',async (req, res) => {
-    let{ username, email, password , age } = req.body;
-let createdUser=await userModel.create({
-username,
-email,
-password,
-age
-})
-res.send(createdUser);
-})
+});
+
+app.post('/create', (req, res) => {
+    let { username, email, password, age } = req.body;
+
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, async (err, hash) => {
+            let createdUser = await userModel.create({
+                username,
+                email,
+                password: hash,
+                age
+            });
+            let token = jwt.sign({ email }, "secretkey")
+            res.cookie('token', token)
+            res.send(createdUser);
+        })
+    })
+});
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
+});
+// invalid email hone per null hoga 
+app.post('/login', async (req, res) => {
+    let user = await userModel.findOne({ email: req.body.email });
+    if (!user)  return res.send(" Invalid email or password");
+
+        // console.log(user.password);
+    
+    // password compare
+    bcrypt.compare(req.body.password, user.password, function (err, result) {
+        if (result) {
+            let token = jwt.sign({email: user.email }, "secretkey")
+            res.cookie('token', token)
+            res.send("Yes You can loggin");
+        }
+        else  res.send("Invalid email or password");
+    })
+ });
+
+
+app.get('/logout', (req, res) => {
+    res.cookie('token', "");
+    res.redirect('/');
+});
+
+
 app.listen(3000);
 
