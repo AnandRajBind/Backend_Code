6e1069dff65dafaaa13766b9e1ebbfb05e779293
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-06-21 17:48:04 +0530

    21/06/2025
    Multer

--- Mini Projec1/app.js
+++ Mini Projec1/app.js
@@ -4,22 +4,51 @@ const cookieParser = require('cookie-parser')
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const path = require('path');
+const multer=require('multer')
 
 const userModel = require('./models/user');
 const postModel = require('./models/post');
 app.set('view engine', 'ejs');
+
+ const crypto=require('crypto');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser());
 
+
+const storage=multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null,'./public/images/upload');
+    },
+
+    filename: function(req, file, cb){
+        crypto.randomBytes(12, function(err, bytes){
+            const fn= bytes.toString("hex")+path.extname(file.originalname);
+            cb(null, fn);
+        })
+    }
+})
+const upload =multer({storage: storage});
+
 app.get('/', (req, res) => {
     res.render('index');
 })
+app.get('/test', (req, res) => {
+    res.render('test');
+})
+app.post('/upload', upload.single('image'), (req, res) => {
+    console.log(req.file);
+
+    res.send("File uploaded successfully");
+})
+
+
 app.get('/login', (req, res) => {
     res.render('login')
 })
+
 app.get('/profile', isLoggedIn, async (req, res) => {
     let user = await userModel.findOne({ email: req.user.email }).populate('posts');
     console.log(user.posts);
