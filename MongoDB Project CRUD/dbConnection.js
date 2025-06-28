36abbe0c2384532050200b3ab5459da3c264cfb1
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-06-28 09:57:56 +0530

    28/06/2025
    Insert and read API

--- MongoDB Project CRUD/dbConnection.js
+++ MongoDB Project CRUD/dbConnection.js
@@ -1,8 +1,8 @@
-const { mongoClient } = require('mongodb');
+// connect mongoDB  with Node js using mongodb package.
+const { MongoClient } = require('mongodb');
 const dbConnectionURL = 'mongodb://127.0.0.1:27017';
 
-const client = new mongoClient(dbConnectionURL);
-
+const client = new MongoClient(dbConnectionURL);
 const dbConnection = async () => {
     await client.connect();
     let db = client.db('Project_DB_Name');
--- MongoDB Project CRUD/index.js
+++ MongoDB Project CRUD/index.js
@@ -1,13 +1,42 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const { dbConnection } = require('./dbConnection');
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/student-read', (req, res) => {
-    res.send(' Student Read API');
+app.get('/student-read', async (req, res) => {
+    let myDB = await dbConnection(); // Connect to the database
+    let studentCollection = myDB.collection("Student_Collection"); // create or get collection
+
+    let data = await studentCollection.find().toArray(); // Fetch all documents from the collection
+    let resObj = {
+        status: '1',
+        msg: 'Student Read API',
+        data
+    }
+    res.send(data);
 });
-app.post('/student-insert', (req, res) => {
-    res.send(' Student Insert API');
+app.post('/student-insert', async (req, res) => {
+    let myDB = await dbConnection();// Connect to the database
+    let studentCollection = myDB.collection("Student_Collection");// create or get collection
+
+    // obj={
+    // name:req.body.name,
+    // email:req.body.email,
+    // }
+
+    let { name, email } = req.body; // Destructuring to get name and email from request body
+    let obj = { name, email }; // Create an object with name and email
+
+    let insertRes = await studentCollection.insertOne(obj); // Insert the object into the collection
+
+    console.log(obj);
+    let resObj = {
+        status: '0',
+        msg: "Data Insert API",
+        insertRes
+    }
+    res.send(resObj); // Send the response back to the client
 });
- app.listen(3000);
+app.listen(3000);
