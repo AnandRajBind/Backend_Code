a68004d17189d36645baf6a77f4183288cb065be
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-06-29 09:08:41 +0530

    29/07/2025
    Same Email Validation and Update (any particular field) validation

--- MongoDB Project CRUD/index.js
+++ MongoDB Project CRUD/index.js
@@ -3,6 +3,7 @@ const { dbConnection } = require('./dbConnection');
 const { ObjectId } = require('mongodb');
 const app = express();
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -30,6 +31,15 @@ app.post('/student-insert', async (req, res) => {
     let { name, email } = req.body; // Destructuring to get name and email from request body
     let obj = { name, email }; // Create an object with name and email
 
+
+    // same email validation
+    let checkEmail = await studentCollection.findOne({ email })
+    if (checkEmail) {
+        return res.send({
+            status: '0',
+            msg: "Email already exists"
+        })
+    }
     let insertRes = await studentCollection.insertOne(obj); // Insert the object into the collection
 
     console.log(obj);
@@ -56,23 +66,33 @@ app.delete("/delete/:id", async (req, res) => {
 })
 
 
+
 app.put("/update/:id", async (req, res) => {
     let { id } = req.params
     let { name, email } = req.body; // Destructuring to get name and email from request body
-    let obj = { name, email }; // data to update
 
+
+    let obj = {}; // data to update
+// empty object validation
+    if (name !== "" && name !== undefined && name !== null) {
+        obj['name'] = name
+    }
+    if (email !== "" && email !== undefined && email !== null) {
+        obj['email'] = email
+    }
+
+    console.log(obj);
 
     let myDB = await dbConnection();
     let studentCollection = myDB.collection("Student_Collection");
 
-    let updateRes = await studentCollection.updateOne({ _id: new ObjectId(id) }, { $set: { name, email } }); // Update the document with the specified id
+    let updateRes = await studentCollection.updateOne({ _id: new ObjectId(id) }, { $set: obj }); // Update the document with the specified id
 
     let resObj = {
         status: '0',
         msg: "Data update API",
         updateRes
     }
-    
-    res.send(updateRes); // Send the response back to the client
+    res.send(resObj); // Send the response back to the client
 })
 app.listen(3000);
